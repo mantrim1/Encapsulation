69138f75457e6f3be1aa6835f4011e1d9c7370b4
Mark <Mark@10.30.18.105>
2015-01-29 16:45:34 -0600

    edited employee class and added hr manager

---
+++ src/lab1/solution/Employee.java
@@ -0,0 +1,77 @@
+package lab1.solution;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * In this lab your challenge is to fix the code in both classes to use
+ * proper encapsulation and the other best practices as explained by 
+ * your instructor.
+ *
+ * @author      Jim Lombardo, WCTC Instructor
+ * @version     1.01
+ */
+public class Employee {
+    private String firstName;
+    private String lastName;
+    private String ssn;
+    private Date birthDate;
+   private HrManager hr;
+
+    public Employee(String fName, String lName,String sSN){
+       firstName=fName;
+       lastName=lName;
+       ssn=sSN;
+   }
+   // Assume this must be performed first
+    private void meetWithHrForBenefitAndSalryInfo() {
+        SimpleDateFormat sdf = new SimpleDateFormat("M/d/yyyy");
+        String fmtDate = sdf.format(currentDate);
+        System.out.println("Met with Hr on " + fmtDate);
+        hr.metWithHr = true;
+    }
+
+    // Assume this is must be performed second
+    private void meetDepartmentStaff() {
+        if(metWithHr) {
+            SimpleDateFormat sdf = new SimpleDateFormat("M/d/yyyy");
+            String fmtDate = sdf.format(currentDate);
+            System.out.println("Met with Dept. Staff on " + fmtDate);
+            metDeptStaff = true;
+        } else {
+            System.out.println("Sorry, you cannot meet with "
+                    + "department staff until you have met with HR.");
+        }
+    }
+
+    // Assume this must be performed third
+    private void reviewDeptPolicies() {
+        if(metWithHr && metDeptStaff) {
+            SimpleDateFormat sdf = new SimpleDateFormat("M/d/yyyy");
+            String fmtDate = sdf.format(currentDate);
+            System.out.println("Reviewed Dept. Policies on " + fmtDate);
+            reviewedDeptPolicies = true;
+        } else {
+            System.out.println("Sorry, you cannot review "
+                    + " department policies until you have first met with HR "
+                    + "and then with department staff.");
+        }
+    }
+
+    // Assume this must be performed 4th
+    private void moveIntoCubicle(String cubeId) {
+        if(metWithHr && metDeptStaff && reviewedDeptPolicies) {
+            SimpleDateFormat sdf = new SimpleDateFormat("M/d/yyyy");
+            String fmtDate = sdf.format(currentDate);
+            System.out.println("Moved into cube on " + fmtDate);
+            this.cubeId = cubeId;
+            this.movedIn = true;
+        } else {
+            System.out.println("Sorry, you cannot move in to a "
+                    + "cubicle until you have first met with HR "
+                    + "and then with department staff, and then reviewed"
+                    + "department policies.");
+        }
+
+    }
+}
